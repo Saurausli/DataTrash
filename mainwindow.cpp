4d5b7464638357d7cde3ef010cadfd216d21681c
Saurausli <51965622+Saurausli@users.noreply.github.com>
2019-08-17 22:17:10 +0200

    random char

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "qglobal.h"
+#include "QDebug"
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -19,8 +20,13 @@ void MainWindow::on_pushButton_clicked()
     ui->status->setText("producingTrash");
     MyFile.open("trash.txt");
     MyFile.clear();
+
+    qsrand(qrand());
     for (unsigned int i=0;i< unsigned(ui->dateGroesse->value());++i) {
-        MyFile << "0";
+
+        int v= qrand()%128;
+        qDebug()<<v<<" is char: "<<char(v);
+        MyFile << char(v);
     }
     ui->status->setText("finished producing Trash");
 }
